9bc310bccd0f6f5b054f632ed2aed360780cfdc3
WalJT <noreply@juliantwalsh.ie>
2024-01-18 20:05:23 +0000

    added basic structure of how plot function will work

--- PyPLANE/webui.py
+++ PyPLANE/webui.py
@@ -5,6 +5,10 @@ TODO: Info about UI
 
 from nicegui import ui
 
+# Function to take data and generate a plot
+def clicked_plot(x_prime: str, y_prime, params: dict = dict()):
+    ui.notification("test")
+
 with ui.row():
     with ui.card():
         """
@@ -15,20 +19,20 @@ with ui.row():
         ui.markdown("# Inputs\n## Equations")
 
         # Inputs for the derivative of x and y
-        x_prime = ui.input(label="x' = ")
-        y_prime = ui.input(label="y' = ")
+        x_prime_input = ui.input(label="x' = ")
+        y_prime_input = ui.input(label="y' = ")
 
         # Inputs for the limits of the x and y axes on the plot that will be generated
         ui.markdown("## Limits of plot axes")
         with ui.row():
-            lower_x_input = ui.input(label="Lower x limit:", placeholder="<a number>")
+            lower_x_input = ui.input(label="Lower x limit:")
             ui.label("< x <")
-            upper_x_input = ui.input(label="Upper x limit:", placeholder="<a number>")
+            upper_x_input = ui.input(label="Upper x limit:")
 
         with ui.row():
-            lower_y_input = ui.input(label="Lower y limit:", placeholder="<a number>")
+            lower_y_input = ui.input(label="Lower y limit:")
             ui.label("< y <")
-            upper_y_input = ui.input(label="Upper y limit:", placeholder="<a number>")
+            upper_y_input = ui.input(label="Upper y limit:")
 
         # Parameters that can be added optionally
         ui.markdown("## Parameters")
@@ -37,7 +41,7 @@ with ui.row():
             ui.label(" = ")
             ui.input(label="Parameter Value", placeholder="<a number>")
         
-        ui.button("Plot")
+        ui.button("Plot", on_click=lambda: clicked_plot(x_prime_input.value, y_prime_input.value))
 
 
     with ui.card():
